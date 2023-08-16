7bcbfe4f998cb56a9a5609f2ab6c41c682439cda
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-16 11:56:43 +0700

    Create hellosmanss.c

---
+++ hellosmanss.c
@@ -0,0 +1,23 @@
+/*
+Program Welcome, hello world
+Buku Siswa IKM Informatika untuk SMA Kelas X 
+Bab 7 Algoritma dan Pemrograman
+2. Struktur Program Bahasa C Hal. 154
+Programmed by : Armansyah, S.Kom, M.Pd
+Guru Informatika SMAN Sumatera Selatan 
+armansyah@smansumsel.sch.id
+Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+Validasi Sertifikat : https://bit.ly/ceksertifikatcs50
+Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
+
+*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+	printf("Hello SMAN Sumsel\nOleh: Armansyah");
+
+}
