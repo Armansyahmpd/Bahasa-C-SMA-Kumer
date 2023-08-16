e9a3c417507681f3577b14f88d04cae18e75e901
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-16 11:58:40 +0700

    Create persamaanvalue.c

---
+++ persamaanvalue.c
@@ -0,0 +1,27 @@
+/*
+Program Persamaan nilai A dan nilai B 
+Programmed by : Armansyah, S.Kom, M.Pd
+Guru Informatika SMAN Sumatera Selatan 
+armansyah@smansumsel.sch.id
+Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+Validasi Sertifikat : https://bit.ly/ceksertifikatcs50
+Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
+
+*/
+
+
+#include <stdio.h>
+int main()
+{
+	int a =20, b =30;  //disini nilai a dan b sudah ditentukan dari dalam program
+		
+	if (a == b) 
+	{
+		printf("a = b \n");
+		return 0;
+	}
+	else
+	printf("a # b \n");
+	return 0;
+	
+}
