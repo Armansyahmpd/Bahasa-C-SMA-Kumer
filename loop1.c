c5b84f3849b05bb48d7b5c585f0b7b548da886b4
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-16 12:12:58 +0700

    Create loop1.c

---
+++ loop1.c
@@ -0,0 +1,23 @@
+/*
+Buku Siswa IKM Informatika untuk SMA Kelas X 
+Bab 7 Algoritma dan Pemrograman Hal. 177
+Pengembangan untuk Struktur Kontrol Perulangan
+Programmed by : Armansyah, S.Kom, M.Pd
+Guru Informatika SMAN Sumatera Selatan 
+armansyah@smansumsel.sch.id
+Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+Validasi Sertifikat : https://bit.ly/ceksertifikatcs50
+Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
+*/
+
+#include <stdio.h>
+int main() {
+int i = 1;
+do 
+  { 
+     printf("Aku Berjanji Tidak akan Mencontek Lagi!\n");
+     i++;
+}
+while(i<=100);
+}
+
