8a94a80fc814bd284d4527f344615d519d9c74e5
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-16 12:01:39 +0700

    Create scanf.c

---
+++ scanf.c
@@ -0,0 +1,26 @@
+/*
+Program Welcome, hello world
+Buku Siswa IKM Informatika untuk SMA Kelas X 
+Bab 7 Algoritma dan Pemrograman
+2. Struktur Program Bahasa C Hal. 154
+Pengembangan untuk pengenalan fungsi scanf
+Programmed by : Armansyah, S.Kom, M.Pd
+Guru Informatika SMAN Sumatera Selatan 
+armansyah@smansumsel.sch.id
+Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+Validasi Sertifikat : https://bit.ly/ceksertifikatcs50
+Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
+*/
+
+#include<stdio.h>  
+
+void main()  
+{
+  char nama[20]; 
+  printf("Whats your name ? "); 
+  scanf("%s",&nama);  
+  
+  printf("Hallo %s", nama);  
+         
+}
+
