212e58e6c76b611e572965250acdfd4097fc81d7
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-08-16 11:45:11 +0700

    Create cetakdesimal.c

---
+++ cetakdesimal.c
@@ -0,0 +1,19 @@
+/*
+Program Cetak Desimal 
+Buku Siswa IKM Informatika untuk SMA Kelas X 
+Bab 7 Algoritma dan Pemrograman Hal. 161
+Programmed by : Armansyah, S.Kom, M.Pd
+Guru Informatika SMAN Sumatera Selatan 
+armansyah@smansumsel.sch.id
+Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+Validasi Sertifikat : https://bit.ly/ceksertifikatcs50
+Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
+
+*/
+
+#include <stdio.h>
+int main() {
+    float x = 12.3456789;
+    printf("%.3f\n", x);
+    return 0;
+}
