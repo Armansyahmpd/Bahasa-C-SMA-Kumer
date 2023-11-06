b2d36d3ca8e13eb9d18be6dfa18c8ba17962bb54
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-11-06 08:53:55 +0700

    Update hitungluaspersegi.c

--- hitungluaspersegi.c
+++ hitungluaspersegi.c
@@ -2,6 +2,11 @@
 Problem 2 : Menghitung Luas Persegi
 Buku Informatika IKM Hal. 169
 Oleh : Armansyah, S.Kom, M.Pd
+Guru Informatika SMAN Sumatera Selatan 
+armansyah@smansumsel.sch.id
+Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+Validasi Sertifikat : https://bit.ly/ceksertifikatcs50
+Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
 
 */
 #include <stdio.h>
