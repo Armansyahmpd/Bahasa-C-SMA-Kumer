34f66f3cca2f90d4499d0dcb9f9d5635e4f372fe
Armansyah, S.Kom, M.Pd <40592177+Armansyahmpd@users.noreply.github.com>
2023-11-06 08:53:14 +0700

    Update menghitungluastanah.c

--- menghitungluastanah.c
+++ menghitungluastanah.c
@@ -2,6 +2,12 @@
 Problem1 : Menghitung Luas Tanah
 Buku Informatika Kelas 10 IKM Hal. 169
 Oleh : Armansyah, S.Kom, M.Pd
+Guru Informatika SMAN Sumatera Selatan 
+armansyah@smansumsel.sch.id
+Alumni CS50 for Teachers Harvard-Indonesia 2022/2023
+Validasi Sertifikat : https://bit.ly/ceksertifikatcs50
+Setiap copy-paste dan pengembangan harus mencantumkan informasi HAKI diatas
+
 */
 
 #include <stdio.h>
